8b65a018cd48edc82d30655d47e0700103d4d9c9
EdgeAIGuy <theedgeaiguy@gmail.com>
2026-07-10 17:07:29 -0500

    add write and read register declarations to i2c header

--- project-3.1-i2c-bmp280/Inc/i2c.h
+++ project-3.1-i2c-bmp280/Inc/i2c.h
@@ -9,5 +9,7 @@ void i2c_stop(void);
 int i2c_send_address(uint8_t addr, uint8_t rw);
 int i2c_write_byte(uint8_t data);
 int i2c_read_byte(uint8_t *data, int ack);
+int i2c_write_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t data);
+int i2c_read_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data);
 
 #endif // I2C_H
